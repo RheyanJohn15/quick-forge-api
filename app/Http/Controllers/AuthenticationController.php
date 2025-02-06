62e145cf28a7e6eb4773deadb15a24c72062d05f
RheyanJohn <rjvblanco.chmsu@gmail.com>
2025-02-07 06:45:51 +0800

    minor work

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -6,6 +6,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Response;
 class AuthenticationController extends Controller
 {
+    /**
+     * Login authentication for the user
+     */
     public function login(Request $req){
         return Response::success([
             'message' => 'return',
