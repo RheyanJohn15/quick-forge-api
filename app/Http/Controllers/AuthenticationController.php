d72afd32092580d4d11a2209e936faaf9864e25b
RheyanJohn <rjvblanco.chmsu@gmail.com>
2025-02-07 06:36:06 +0800

    Updated some teesting api

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Response;
 class AuthenticationController extends Controller
 {
     public function login(Request $req){
-
+        return Response::success([
+            'message' => 'return',
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,4 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthenticationController;
 
-Route::post('/login', [AuthenticationController::class, 'login']);
+Route::get('/login', [AuthenticationController::class, 'login']);
